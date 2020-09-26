471597fa393ea64822083edd3aff3d65b2f4aa19
Aguaviva, Raul <raul.aguaviva@amd.com>
2020-09-26 15:32:45 +0200

    removing ord

--- micro-jpeg-visualizer.py
+++ micro-jpeg-visualizer.py
@@ -99,7 +99,7 @@ class Stream:
 		self.pos = 0
 
 	def GetBit(self):
-		b = ord(self.data[self.pos >> 3])
+		b = self.data[self.pos >> 3]
 		s = 7-(self.pos & 0x7)
 		self.pos+=1
 		return (b >> s) & 1
